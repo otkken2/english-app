d0f2e403f7bce629e756987a0b2510db2a346697
otkken2 <lovelovezamakko2@gmail.com>
2024-07-13 22:00:37 +0900

    コンポーネント化して整理

---
+++ src/pages/api/components/WordsArea.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { WordCard } from './WordCard'
+
+interface Props {
+  words: string[]
+  handleSelectWord: (word: string) => void
+}
+
+export const WordsArea = ({ words, handleSelectWord }: Props) => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full'>
+        {words.map((word, index) => (
+          <div key={index} onClick={() => handleSelectWord(word)} className='w-full'>
+            <WordCard word={word} />
+          </div>
+        ))}
+      </div>
+  )
+}
+
+export default WordsArea
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { WordCard } from './api/components/WordCard'
+import WordsArea from './api/components/WordsArea'
+import { InputArea } from './api/components/InputArea'
+import { AnswerButton } from './api/components/AnswerButton'
 
 const words = ['this', 'pen', 'a', 'is']
 const answer = 'this is a pen'
@@ -19,21 +22,10 @@ const Home = () => {
   }
   return (
     <div className='flex flex-col items-center md:max-w-[640px] max-w-screen w-full p-10 gap-10'>
-      <div className='w-full h-1/2 flex flex-col gap-4 justify-center items-center'>
-        <h1 className='text-4xl font-bold'>English App</h1>
-        <div className='flex gap-4 w-full'>
-          <input type="text" value={inputText} className='md:w-[500px] w-full h-10 p-2  bg-inherit border-b-2 border-gray-300' />
-          <button onClick={() => setInputText('')} className='bg-red-500 rounded-md p-2 text-sm text-gray-800'>clear</button>
-        </div>
-      </div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full'>
-        {words.map((word, index) => (
-          <div key={index} onClick={() => handleSelectWord(word)} className='w-full'>
-            <WordCard word={word} />
-          </div>
-        ))}
-      </div>
-      <button onClick={handleSubmit} className='bg-blue-500 text-white p-2 rounded-md w-full md:w-fit'>解答する</button>
+      <h1 className='text-4xl font-bold'>English App</h1>
+      <InputArea inputText={inputText} setInputText={setInputText} />
+      <WordsArea words={words} handleSelectWord={handleSelectWord} />
+      <AnswerButton handleSubmit={handleSubmit} />
     </div>
   )
 }
