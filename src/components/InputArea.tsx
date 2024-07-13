2f3dd568d504eee2764301c26cc9f7517cc52b68
otkken2 <lovelovezamakko2@gmail.com>
2024-07-14 07:57:24 +0900

    疑問文の場合は文末にカンマを表示しないよう修正

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -5,9 +5,17 @@ interface Props {
   setInputText: (inputText: string) => void
 }
 export const InputArea = ({ inputText }: Props) => {
+  const handleSentenceEnding = (inputText: string) => {
+    if(!inputText) return ""
+    if(inputText.includes("?")){
+      return ""
+    } else {
+      return "."
+    }
+  }
   return (
     <div className='w-full flex flex-col gap-4 justify-center items-center'>
-        <input type="text" value={`${inputText}${inputText ? "." : ""}`} className='w-full h-10 p-2 text-lg bg-inherit border-b-2 border-gray-300' />
+        <input type="text" value={`${inputText}${handleSentenceEnding(inputText)}`} className='w-full h-10 p-2 text-lg bg-inherit border-b-2 border-gray-300' />
     </div>
   )
 }
